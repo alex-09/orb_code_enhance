d663680631994d81a5b960277ed5867c22ecb1fd
alex-09 <alexandeilingad@gmail.com>
2025-01-12 23:48:29 +0800

    Created the functions of enhanced ORB (Amadeo-Bautista-Lingad) to be used for App Development

---
+++ AppFunctions/preprocess.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+'''
+    START: PREPROCESSING
+        combining these steps:
+        - grayscale
+        - gaussian blur
+        - clahe
+        - canny edge detection
+'''
+def preprocess_image(image):
+    # Convert to Grayscale (for dimensionality reduction)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian Blur (to reduce noise)
+    blurred_image = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Apply CLAHE for contrast enhancement
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(4, 4))
+    clahe_apply = clahe.apply(blurred_image)
+    
+    # Apply Canny Edge Detection (parameter adjusted to enhance contrast)
+    edges = cv2.Canny(clahe_apply, threshold1=100, threshold2=100)
+
+    combined_edges_clahe = cv2.addWeighted(clahe_apply, 0.8, edges, 0.1, 0.8)
+
+    return combined_edges_clahe
+
+def preprocess_image_estacio_laurente(image):
+    scale_percent = 100
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+    clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8, 8))
+    clahe_apply = clahe.apply(image)
+
+    return clahe_apply
+
+def preprocess_none(image):
+    # Convert to Grayscale (for dimensionality reduction)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
+  
+
--- README.md
+++ README.md
@@ -1,23 +1,4 @@
-# Just a quick note
----
-1. The `main.py` is where we will conduct the tests.
-2. The `match.py` is the main function to use ORFLANSAC, EST-LAU ORB, and modified versions of those algorithms (through parameter changes).
-    - Imports of needed processes and algorithms are done here.
-3. Note that there are bits of code that must be deleted (or archived) after tests are done. Possible files are:
-    - `match.py`
-    - `ofs.py`
-4. The `ofs.py` is utilized by `match.py`. It is where the main processes of the algorithm lives. These processes or algorithms include:
-    - image pre-processing 
-    - Bayesian Optimization
-    - ORB
-    - MAGSAC++
-    - specialized conditions (for TESTING)
-5. DO NOT USE IT YET (under development): The `window.py` is where our GUI for simulation lives. 
-    - It could also be re-used and modified for the GUI of the PRODUCT SEARCH project.
-
-# Changes (Nov. 17, 2024)
----
-1. Created a separate `orflansac.py` file. 
-2. Corrected test cases.
-3. Deleted the images of `creamo` because it is not part of the obtained dataset from Roboflow.
-4. Updated this README.md file
+# Reminder
+1. Use only the AppFunctions.
+2. The `main.py` will give you an idea on how to use the AMADEO-BAUTISTA-LINGAD ORB algorithm.
+3. Feel free to ask questions about the things that you do not understand.
